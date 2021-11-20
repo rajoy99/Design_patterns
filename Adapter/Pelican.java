62387bf7ebfcac126ca99ee0ae830bac4d9d9332
Rafat Ashraf Joy <61883509+rajoy99@users.noreply.github.com>
2021-11-20 22:39:46 +0600

    Add files via upload

---
+++ Adapter/Pelican.java
@@ -0,0 +1,11 @@
+public class Pelican implements Bird {
+
+    public void fly() {
+        System.out.println("Pelican is Flying");
+    }
+
+    public void chrip(){
+        System.out.println("A bird chriping sound");
+    }
+
+}
