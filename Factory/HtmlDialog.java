c4f389e7cc396ac9c2e5a6c419b2e07e42d0f3cc
Rafat Ashraf Joy <61883509+rajoy99@users.noreply.github.com>
2021-11-20 22:43:34 +0600

    Add files via upload

---
+++ Factory/HtmlDialog.java
@@ -0,0 +1,10 @@
+/**
+ * HTML Dialog will produce HTML buttons.
+ */
+public class HtmlDialog extends Dialog {
+
+    @Override
+    public Button createButton() {
+        return new HtmlButton();
+    }
+}
---
+++ Factory/WindowsDialog.java
@@ -0,0 +1,10 @@
+/**
+ * Windows Dialog will produce Windows buttons.
+ */
+public class WindowsDialog extends Dialog {
+
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
+}
