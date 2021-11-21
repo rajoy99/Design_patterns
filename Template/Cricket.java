38cda9835acce8250264cfeeb484e9a8c262ee3f
Rafat Ashraf Joy <61883509+rajoy99@users.noreply.github.com>
2021-11-21 11:12:30 +0600

    Add files via upload

---
+++ Template/Cricket.java
@@ -0,0 +1,16 @@
+public class Cricket extends Skeleton {
+
+    public void initialize(){
+        System.out.println("A cricket game has been initialized");
+    }
+
+    public void start(){
+        System.out.println("The Cricket game just started ");
+    }
+
+    public void end(){
+        System.out.println("The Cricket game Ended ! ");
+    }
+
+
+}
