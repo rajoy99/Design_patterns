39f92f764412360d9e8a2f23d3f01d9ac4928bb2
Rafat Ashraf Joy <61883509+rajoy99@users.noreply.github.com>
2021-11-21 11:12:44 +0600

    Add files via upload

---
+++ Template/Hockey.java
@@ -0,0 +1,16 @@
+public class Hockey extends Skeleton {
+
+    public void initialize(){
+        System.out.println("A Hockey game has been initialized");
+    }
+
+    public void start(){
+        System.out.println("The Hockey game just started ");
+    }
+
+    public void end(){
+        System.out.println("The Hockey game Ended ! ");
+    }
+
+
+}
